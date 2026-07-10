2c8913e86c2a14c0dc4a2c2bbd33747e948d384c
Numtthanon Sukjam <2513110482@tni.ac.th>
2026-07-10 11:08:26 +0700

    commit

---
+++ LabSheet02/src/Ex03.java
@@ -0,0 +1,22 @@
+public class Ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayController nums = new ArrayController();
+		nums.display("Display an inital array: ");
+		
+		int[] new_nums = new int[nums.getNums().length+1];
+		
+		for (int i=0; i<nums.getNums().length; i++) {
+			new_nums[i] = nums.getNums()[i];
+		}
+		
+		nums.setNums(new_nums);
+		nums.display("\nAfter copy element");
+		
+		nums.getNums()[nums.getNums().length-1] = 15;
+		nums.display("\nAfter added the last element:");
+
+	}
+
+}
