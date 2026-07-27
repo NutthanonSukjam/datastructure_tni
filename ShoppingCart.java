45ac0e0c06105688c7306e4a3de6ccc09da87adc
NutthanonSukjam <cat.nutthanonsukjam@gmail.com>
2026-07-27 13:41:16 +0700

    2513110482 Submit Labsheet04

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,32 +2,32 @@ import java.util.LinkedList;
 
 public class ShoppingCart {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        LinkedList<String> cart =  new LinkedList<String>();
 
-		LinkedList<String> cart = new LinkedList<String>();
-		
-		cart.add("Wireless Mouse");
-		cart.add("Mechanical Keyboard");
-		cart.add("USB-C Hub");
-		System.out.println(cart);
-		
-		cart.add(1, "Mouse Pad");
-		System.out.println(cart);
-		
-		cart.removeLast();
-		System.out.println(cart);
-		
-		cart.remove("Mouse Pad");
-		System.out.println(cart);
-		
-		cart.add("Monitar");
-		System.out.println(cart);
-		
-		cart.set(2, "Phone Holder");
-		System.out.println(cart);
-		
-		System.out.println(cart.getLast());
-	}
+        cart.add("Wireless Mouse");
+        cart.add("Mechanical Keyboard");
+        cart.add("USB-C Hub");
+        System.out.println(cart);
+
+
+        cart.add(1,"Mouse Pad");
+        System.out.println(cart);
+
+        cart.removeLast();
+        System.out.println(cart);
+
+        cart.remove("Mouse Pad");
+        System.out.println(cart);
+
+        cart.add("Monitor");
+        System.out.println(cart);
+
+        cart.set(2, "Phone Holder");
+        System.out.println(cart);
+
+        System.out.println(cart.getLast());
+
+    }
 
 }
--- TodoList.java
+++ TodoList.java
@@ -1,26 +1,31 @@
-
 public class TodoList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        DoublyLinkedList todolist = new DoublyLinkedList();
 
-		DoublyLinkedList todolist = new DoublyLinkedList();
-		
-		todolist.insert("Finish Homework");
-		System.out.println(todolist.traversal());
-		
-		todolist.insert("Laundry");
-		todolist.insert("Group meeting");
-		System.out.println(todolist.traversal());
-		
-		todolist.insert(0,"Submit Report");
-		System.out.println(todolist.traversal());
-		
-		todolist.insert(2, "Buy Food");
-		System.out.println(todolist.traversal());
-		
-		todolist.insert("Go to Gym");
-		System.out.println(todolist.traversal());
-	}
+        todolist.insert("Finish Homework");
+        todolist.insert("Laundry");
+        todolist.insert("Group Meeting");
+        System.out.println("To-Do List = " + todolist.traversal());
 
+        todolist.insert(0, "Submit Report");
+        System.out.println("To-Do List = " + todolist.traversal());
+
+        todolist.insert(2, "Buy Food");
+        System.out.println("To-Do List = " + todolist.traversal());
+
+        todolist.insert("Go to Gym");
+        System.out.println("To-Do List = " + todolist.traversal());
+
+        todolist.remove();
+
+        todolist.remove(3);
+
+        todolist.remove(0);
+
+        System.out.println("To-Do List = " + todolist.traversal());
+
+        System.out.println();
+        System.out.println("Backward Traversal = " + todolist.backwardTraversal());
+    }
 }
