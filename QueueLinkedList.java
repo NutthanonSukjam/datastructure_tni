62a16e5d48f41f2acc7ec1fc786f9f3d82e2881b
NutthanonSukjam <cat.nutthanonsukjam@gmail.com>
2026-08-03 16:47:14 +0700

    2513110482 submit Labsheet06

---
+++ QueueLinkedList.java
@@ -0,0 +1,41 @@
+public class QueueLinkedList {
+
+    private Node front;
+    private Node rear;
+
+    public QueueLinkedList() {
+        front = null;
+        rear  = null;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public void enqueue(int value) {
+        Node new_node = new Node(value);
+        if (isEmpty()) {
+            front = new_node;
+            rear = new_node;
+        } else {
+            rear.next = new_node;
+            rear = new_node;
+        }
+    }
+
+    public int peek() {
+        return !isEmpty()? front.data : -1;
+    }
+
+    public int dequeue() {
+        if (!isEmpty()) {
+            Node temp_node = front; // back up front node
+            int temp_data = temp_node.data; // back up data in front node
+            front = front.next; //move front
+            temp_node = null; //delete front
+            return temp_data;
+        }
+        return -1;
+    }
+
+}
